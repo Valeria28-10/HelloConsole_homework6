64843169bbeff9db7dd9a0b2bd2a4faf9479092d
Valeria28-10 <valeriakonovalenko828@gmail.com>
2023-05-29 13:07:14 +0300

    ДЗ_6 Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите любые числа через пробел или запятую: ");
+char[] NumbersToSelect = new char[] {',', ' '};
+string[] numbers = Console.ReadLine().Split(NumbersToSelect, StringSplitOptions.RemoveEmptyEntries);
+int[] num = Array.ConvertAll(numbers, int.Parse);
+int count = 0;
+for (int i =0; i < numbers.Length; i++)
+{
+    if (num[i] > 0)
+    {
+        count ++;
+    }
+}
+Console.WriteLine($"Количество чисел больше нуля: {count}");
+
